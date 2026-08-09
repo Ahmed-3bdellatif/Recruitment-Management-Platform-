22b8b47b05381e82f0b39fc844daf1fee67b9386
Ahmed-3bdellatif <ahmedabdellatif5355@gmail.com>
2026-08-09 15:48:29 +0300

    implementing Candidate Repo

---
+++ src/main/java/recruitmentmanagmentplatform/recruitmentmanagementplatform/candidate/CandidateRepository.java
@@ -0,0 +1,6 @@
+package recruitmentmanagmentplatform.recruitmentmanagementplatform.candidate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<Candidate, Long> {
+}
