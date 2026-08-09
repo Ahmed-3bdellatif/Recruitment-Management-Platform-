07cbd4e67617234fe655e0040f2b9a6ba6da9251
Ahmed-3bdellatif <ahmedabdellatif5355@gmail.com>
2026-08-09 15:51:02 +0300

    Application repository done

---
+++ src/main/java/recruitmentmanagmentplatform/recruitmentmanagementplatform/application/ApplicationRepository.java
@@ -0,0 +1,6 @@
+package recruitmentmanagmentplatform.recruitmentmanagementplatform.application;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+}
