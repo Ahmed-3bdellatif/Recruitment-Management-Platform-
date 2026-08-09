32822431ba2876575e8b4f50dca24510502d01fd
Ahmed-3bdellatif <ahmedabdellatif5355@gmail.com>
2026-08-09 15:55:15 +0300

    interview repository add

---
+++ src/main/java/recruitmentmanagmentplatform/recruitmentmanagementplatform/interview/InterviewRepository.java
@@ -0,0 +1,6 @@
+package recruitmentmanagmentplatform.recruitmentmanagementplatform.interview;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterviewRepository extends JpaRepository<Interview, Long> {
+}
