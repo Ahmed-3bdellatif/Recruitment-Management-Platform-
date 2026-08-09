4be9d95f31a6436d7814833735a9125588a18c23
Ahmed-3bdellatif <ahmedabdellatif5355@gmail.com>
2026-08-09 15:38:47 +0300

    Created Job Repository

---
+++ src/main/java/recruitmentmanagmentplatform/recruitmentmanagementplatform/job/JobRepository.java
@@ -0,0 +1,6 @@
+package recruitmentmanagmentplatform.recruitmentmanagementplatform.job;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Job, Long> {
+}
