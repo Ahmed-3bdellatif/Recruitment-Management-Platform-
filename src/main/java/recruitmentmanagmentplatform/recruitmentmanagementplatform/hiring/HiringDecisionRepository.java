13f2f808735a5aac382916dc06b4706fea113ee4
Ahmed-3bdellatif <ahmedabdellatif5355@gmail.com>
2026-08-09 15:42:07 +0300

    Hiring Decision Repo Created

---
+++ src/main/java/recruitmentmanagmentplatform/recruitmentmanagementplatform/hiring/HiringDecisionRepository.java
@@ -0,0 +1,6 @@
+package recruitmentmanagmentplatform.recruitmentmanagementplatform.hiring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HiringDecisionRepository extends JpaRepository<HiringDecision, Long> {
+}
